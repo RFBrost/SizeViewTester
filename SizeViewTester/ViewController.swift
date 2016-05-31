6fe916c005d0daa84d350411687df77ca95e7a06
Robert F. Brost <RF@Brost.net>
2016-05-31 00:42:15 -0700

    Initial Commit

---
+++ SizeViewTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeViewTester
+//
+//  Created by Robert Brost on 05/31/16.
+//  Copyright © 2016 RFBrost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
