717427907130015dab1a00225a25115ce2bf9c4c
Robert F. Brost <RF@Brost.net>
2016-07-04 01:56:10 -0700

    Got SizeTester working w/Icon set and NO errors!

--- SizeViewTester/AppDelegate.swift
+++ SizeViewTester/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // ----- AppDelegate: application(UIApplication,[NSObject:AnyObject]?)->Bool
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
         print("AppDelegate: Application Finishing Launching.")
@@ -27,8 +28,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // ----------------- AppDelegate: applicationWillResignActive(UIApplication)
 
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Sent when the application is about to move from active to inactive state. 
+        // This can occur for certain types of temporary interruptions (such as 
+        // an incoming phone call or SMS message) or when the user quits the 
+        // application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle 
+        // down OpenGL ES frame rates. Games should use this method to pause the game.
         
         print("AppDelegate: Application Will Resign Active")
     }   //  end of applicationWillResignActive(UIApplication)
@@ -36,8 +41,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // --------------- AppDelegate: applicationDidEnterBackground(UIApplication)
     
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // Use this method to release shared resources, save user data, 
+        // invalidate timers, and store enough application state information to 
+        // restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is 
+        // called instead of applicationWillTerminate: when the user quits.
         
         print("AppDelegate: Application Did Enter Background")
     }   //  end of applicationDidEnterBackground(UIApplication)
@@ -45,7 +53,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // -------------- AppDelegate: applicationWillEnterForeground(UIApplication)
     
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the inactive 
+        // state; here you can undo many of the changes made on entering the background.
         
         print("AppDelegate: Application Will Enter Foreground")
     }   //  end of applicationWillEnterForeground(UIApplication)
@@ -53,14 +62,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // ------------------ AppDelegate: applicationDidBecomeActive(UIApplication)
     
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Restart any tasks that were paused (or not yet started) while the 
+        // application was inactive. If the application was previously in the 
+        // background, optionally refresh the user interface.
         print("AppDelegate: Application Did Become Active")
     }   //  end of applicationDidBecomeActive(UIApplication)
 
     // -------------------- AppDelegate: applicationWillTerminate(UIApplication)
     
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate. Save data if 
+        // appropriate. See also applicationDidEnterBackground:.
         print("Application Will Terminate")
     }   //  end of applicationWillTerminate(UIApplication)
     
--- SizeViewTester/ViewController.swift
+++ SizeViewTester/ViewController.swift
@@ -8,6 +8,186 @@
 
 import UIKit
 
+internal var viewControllerRef: TestViewController? = nil
+
+// ---------------------- sizeClassDescription(UIUserInterfaceSizeClass)->String
+
+private func sizeClassDescription(sizeClass: UIUserInterfaceSizeClass)->String {
+    switch sizeClass {
+    case .Compact:
+        return "Compact"
+    case .Regular:
+        return "Regular"
+    case .Unspecified:
+        return "Unspecified"
+    }   // end of switch
+}   // end of func sizeClassDescription(UIUserInterfaceSizeClass)->String
+
+// ------------- userInterfaceIdiomDescription(UIUserInterfaceSizeClass)->String
+
+private func userInterfaceIdiomDescription(userDevice: UIUserInterfaceIdiom)->String {
+    switch userDevice {
+    case .Phone:
+        return "Phone"
+    case .Pad:
+        return "Pad"
+    case .TV:
+        return "TV"
+    case .CarPlay:
+        return "CarPlay"
+    case .Unspecified:
+        return "Unspecified"
+    }   // end of switch
+}   // end of func userInterfaceIdiomDescription(UIUserInterfaceIdiom)->String
+
+// ----------------- userInterfaceIdiomDescription(UIUserInterfaceIdiom)->String
+
+private func forceTouchDescription(forceTouchCap: UIForceTouchCapability)->String {
+    switch forceTouchCap {
+    case .Available:
+        return "Available"
+    case .Unavailable:
+        return "Unavailable"
+    case .Unknown:
+        return "Unknown"
+    }   // end of switch
+}   // end of userInterfaceIdiomDescription(UIUserInterfaceIdiom)->String
+
+// ------------------------- orientationDescription(UIDeviceOrientation)->String
+
+private func orientationDescription(orientation: UIDeviceOrientation)->String {
+    switch orientation {
+    case .Portrait:
+        return "Portrait"
+    case .PortraitUpsideDown:
+        return "PortraitUpsideDown"
+    case .LandscapeLeft:
+        return "LandscapeLeft"
+    case .LandscapeRight:
+        return "LandscapeRight"
+    case .FaceUp:
+        return "FaceUp"
+    case .FaceDown:
+        return "FaceDown"
+    case .Unknown:
+        return "Unknown"
+    }   //  end of switch
+}   // end of func orientationDescription(UIDeviceOrientation)->String
+
+// ------------------------------------------ orientation()->UIDeviceOrientation
+
+private func orientation()->UIDeviceOrientation {
+    let myDevice = UIDevice.currentDevice()
+    return myDevice.orientation
+}   //  end of func orientation()->UIDeviceOrientation
+
+// ------------ sizeClass()->(UIUserInterfaceSizeClass,UIUserInterfaceSizeClass)
+
+private func sizeClass()->(horizonal: UIUserInterfaceSizeClass,
+                           vertical: UIUserInterfaceSizeClass   ) {
+        if viewControllerRef != nil {
+            let hSizeClass = viewControllerRef!.traitCollection.horizontalSizeClass
+            let vSizeClass = viewControllerRef!.traitCollection.verticalSizeClass
+            return (hSizeClass,vSizeClass)
+        }
+        else {
+            return (.Unspecified, .Unspecified)
+        }
+}   //  end of func sizeClass()->(UIUserInterfaceSizeClass,UIUserInterfaceSizeClass)
+
+// -------------------------------------------------------------- size()->CGSize
+
+private func size()->CGSize {
+        if let size = viewControllerRef?.view.bounds.size {
+            return size
+        }
+        else {
+            return CGSizeMake(0.0, 0.0)
+        }
+}   //  end of func size()->CGSize
+
+// ------------------------------------------- userIdiom()->UIUserInterfaceIdiom
+
+private func userIdiom()->UIUserInterfaceIdiom {
+    if viewControllerRef != nil {
+        let uIdiom = viewControllerRef!.traitCollection.userInterfaceIdiom
+        return uIdiom
+    }
+    else {
+        return .Unspecified
+    }
+}   //  end of func userIdiom()->UIUserInterfaceIdiom
+
+// ----------------------------------------------------- displayScale()->CGFloat
+
+private func displayScale()->CGFloat {
+    if viewControllerRef != nil {
+        let scale = viewControllerRef!.traitCollection.displayScale
+        return scale
+    }
+    else {
+        return 0.0
+    }
+}   //  end of func displayScale()->CGFloat
+
+// ---------------------------------------- forceTouch()->UIForceTouchCapability
+
+private func forceTouch()->UIForceTouchCapability {
+    if viewControllerRef != nil {
+        let fTouch = viewControllerRef!.traitCollection.forceTouchCapability
+        return fTouch
+    }
+    else {
+        return .Unknown
+    }
+}   //  end of func forceTouch()->UIForceTouchCapability
+
+// ------------------------------------------------------------- displayTraits()
+
+private func displayTraits() {
+    print("Device orientation is: \(orientationDescription(orientation()))")
+    print("Current screen size is:\(size())")
+    
+    let (hSize, vSize) = sizeClass()
+    print("Current horizontalSizeClass is: \(sizeClassDescription(hSize))")
+    print("Current verticalSizeClass is: \(sizeClassDescription(vSize))")
+    print("Current userInterfaceIdiom is: \(userInterfaceIdiomDescription(userIdiom()))")
+    print("Current displayScale is: \(displayScale())")
+    print("Current forceTouchCapability is: \(forceTouchDescription(forceTouch()))")
+    
+    switch vSize {
+    case .Compact:
+        switch hSize {
+        case .Compact:
+            print("wChC - iPhone landscape")
+        case .Regular:
+            print("wRhC - iPhone 6+ landscape")
+        case .Unspecified:
+            print("wUhC")
+        }   //  end of Compact verticalSizeClass
+        
+    case .Regular:
+        switch hSize {
+        case .Compact:
+            print("wChR - iPhone, iPhone 6+ portrait")
+        case .Regular:
+            print("wRhR - iPad portrait, landscape")
+        case .Unspecified:
+            print("wUhR")
+        }   //  end of Regular verticalSizeClass
+        
+    case .Unspecified:
+        switch hSize {
+        case .Compact:
+            print("wChU")
+        case .Regular:
+            print("wRhU")
+        case .Unspecified:
+            print("wUhU")
+        }   //  end of Unspecified verticalSizeClass
+    }   //  end of switch
+}   //  end of func displayTraits()
+
 // ---------------------------------------------------- class TestViewController
 
 class TestViewController: UIViewController {
@@ -52,7 +232,6 @@ class TestViewController: UIViewController {
     //  iPhone iPad Landscape:  Regular Width - Regular Height
     //  iPhone iPad Portrait:   Regular Width - Regular Height
     
-
     
     // ------------------- TestViewController: willTransitionToTraitCollection()
     
@@ -63,9 +242,15 @@ class TestViewController: UIViewController {
         super.willTransitionToTraitCollection(newCollection,
                                               withTransitionCoordinator: coordinator)
         print("TestViewController: View will Transition To Trait Collection.")
+        if self.traitCollection.horizontalSizeClass != newCollection.horizontalSizeClass {
+            print("The horizontalSizeClass DID change!")
+        }
 
+        if self.traitCollection.verticalSizeClass != newCollection.verticalSizeClass {
+            print("The verticalSizeClass DID change!")
+        }
         
-    }   //  end of willTransitionToTraitCollection(UITraitCollection,
+    }   //  end of func willTransitionToTraitCollection(UITraitCollection,
         //     withTransitionCoordinator: UIViewControllerTransitionCoordinator)
     
     // -------------------------- TestViewController: viewWillTransitionToSize()
@@ -75,9 +260,11 @@ class TestViewController: UIViewController {
                                         UIViewControllerTransitionCoordinator) {
         
         super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
-        print("TestViewController: View Will Transition To Size.")
+        
+        print("TestViewController: View Will Transition To Size(\(size)).")
+        displayTraits()
     
-    }   //  end of viewWillTransitionToSize (CGSize, withTransitionCoordinator:
+    }   //  end of func viewWillTransitionToSize (CGSize, withTransitionCoordinator:
         //                                  UIViewControllerTransitionCoordinator)
 
     // -------------------------- TestViewController: traitCollectionDidChange()
@@ -89,8 +276,37 @@ class TestViewController: UIViewController {
         //  if ((self.traitCollection.verticalSizeClass != previousTraitCollection.verticalSizeClass)
         //    || self.traitCollection.horizontalSizeClass != previousTraitCollection.horizontalSizeClass)) {
         // your custom implementation here
-        print("TestViewController: View Trait Collection Did Change.")
         
+        print("TestViewController: View Trait Collection Did Change.")
+        if previousTraitCollection != nil {
+            print("Previous horizontalSizeClass is: \(sizeClassDescription(previousTraitCollection!.horizontalSizeClass))")
+            if self.traitCollection.horizontalSizeClass != previousTraitCollection!.horizontalSizeClass {
+                print("The horizontalSizeClass DID change!")
+            }   // end of if self
+            
+            print("Previous verticalSizeClass is: \(sizeClassDescription(previousTraitCollection!.verticalSizeClass))")
+            if self.traitCollection.verticalSizeClass != previousTraitCollection!.verticalSizeClass {
+                print("The verticalSizeClass DID change!")
+            }   // end of if self
+            
+            print("Previous displayScale is: \(previousTraitCollection!.displayScale)")
+            if self.traitCollection.displayScale != previousTraitCollection!.displayScale {
+                print("The displayScale DID change!")
+            }   // end of if self
+            
+            print("Previous userInterfaceIdiom is: \(userInterfaceIdiomDescription(previousTraitCollection!.userInterfaceIdiom))")
+            if self.traitCollection.userInterfaceIdiom != previousTraitCollection!.userInterfaceIdiom {
+                print("The userInterfaceIdiom DID change!")
+            }   // end of if self
+            
+            print("Previous forceTouchCapability is: \(forceTouchDescription(previousTraitCollection!.forceTouchCapability))")
+            if self.traitCollection.forceTouchCapability != previousTraitCollection!.forceTouchCapability {
+                print("The forceTouchCapability DID change!")
+            }   // end of if self
+        }   //  end of if previousTraitCollection
+        
+       displayTraits()
+     
     }   //  end of func traitCollectionDidChange(UITraitCollection?)
     
     // --------------------------------------- TestViewController: viewDidLoad()
@@ -99,7 +315,8 @@ class TestViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("TestViewController: View Did Load")
-        
+        viewControllerRef = self
+        displayTraits()
     }   //  end of func viewDidLoad()
 
     // --------------------------- TestViewController: didReceiveMemoryWarning()
